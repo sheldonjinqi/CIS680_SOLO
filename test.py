7e2c8e3b78d61fcde120ae76d59065924968515a
SheldonJin <sheldonjinqi@gmail.com>
2020-10-20 15:31:07 -0400

    Update test.py
    Tested both focal and mask loss calculation with dataset provided by TA. Both results match with ground truth.

--- test.py
+++ test.py
@@ -5,6 +5,8 @@ import torch
 def test_loss(solo_head_net):
     focal_input_path = "./data/focal_input.npz"
     focal_output_path = "./data/focal_output.npz"
+    dice_in_path = "./data/dice_input.npz"
+    dice_gt_path = "./data/dice_output.npz"
 
     focal_in = np.load(focal_input_path, allow_pickle= True)
     cate_preds = torch.from_numpy(focal_in['cate_preds'])
@@ -12,15 +14,40 @@ def test_loss(solo_head_net):
     focal_out_gt = np.load(focal_output_path,allow_pickle= True)
     cate_loss = focal_out_gt['cate_loss']
 
-    print('cate_preds',cate_preds.shape)
-    print('cate_gts',cate_gts.shape)
+    dice_in = np.load(dice_in_path,allow_pickle=True)
+    dice_out = np.load(dice_gt_path,allow_pickle=True)
+    print(dice_in.files)
+    print(dice_out.files)
+    print((dice_in['ins_preds']).shape)
+    # print(dice_in['ins_gts'])
+    ins_preds = dice_in['ins_preds']
+    ins_gts = dice_in['ins_gts']
+    ins_loss = dice_out['mask_loss']
+
+
 
     gt = torch.tensor([[0,0,1,],[0,1,1],[0,0,1]])
     gt = torch.unsqueeze(gt, dim=0)
     pred = torch.tensor([[0.2,0.3,0.9],[0.6,0.5,0.7],[0.4,0,0.2]])
     pred = torch.unsqueeze(pred,dim=0)
     dmask = solo_head_net.DiceLoss(pred,gt)
-    print('Dice loss', dmask)
+
+    # print(ins_preds)
+    # print(ins_gts)
+    #
+    # dice_loss_test = solo_head_net.DiceLoss(ins_preds,ins_gts)
+    # print('')
+    # print('Dice loss input', dice_loss_test)
+    dice_loss_sum = 0
+    N_total = 0
+    num_fpn = 5
+    for i in range(num_fpn):
+        dice_loss_sum += solo_head_net.DiceLoss(torch.from_numpy(ins_preds[i]), torch.from_numpy(ins_gts[i]))
+        N_total += ins_preds[i].shape[0]
+    mask_loss = dice_loss_sum / N_total
+    print('Mask loss calculated', mask_loss*3)
+    print('Mask loss gt', ins_loss)
+    # print('Dice loss', dmask)
 
     simple_focal_pred = torch.tensor([[0.8,0.3,0.6],[0.1,0.2,0.5],[0.7,0.4,0.4]])
     # print('simple_focal_pred',simple_focal_pred)
@@ -31,6 +58,8 @@ def test_loss(solo_head_net):
     print('Calculated focal loss', focal_loss_test)
     print('Ground Truth focal loss',cate_loss)
     print('Focal loss simple', focal_loss_simple)
+
+
 
     print('completed test')
 
